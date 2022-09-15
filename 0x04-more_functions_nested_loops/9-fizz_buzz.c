042ff9502e77acde30e453266d9c14d301a3b819
edmundspira <edmunddspira@gmail.com>
2022-09-15 16:39:04 -0700

    9-fizz_buzz.c FIX

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,35 +11,27 @@
 
 int main(void)
 {
-	int num = 1;
+	int i;
 
-	while (num++ < 100)
+	for (i = 1; i <= 100; i++)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
+		if (i % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
-		else if ((num % 3) == 0)
+		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
-		else if ((num % 5) == 0)
+		else if (i % 5 == 0)
 		{
-			if (num != 100)
-			{
-				printf("Buzz ")
-			}
-			else
-			{
-				printf("Buzz");
-			}
+			printf("Buzz");
 		}
 		else
-		{
-			printf("%d ", num);
-		}
+			printf("%i", i);
+		if (i < 100)
+			printf(" ");
 	}
 	printf("\n");
-
 	return (0);
 }
