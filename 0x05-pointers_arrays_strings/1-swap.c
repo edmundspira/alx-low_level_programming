c7fce9f9acdf6bc87c5fb1af042f079ec3cfeb26
edmundspira <edmunddspira@gmail.com>
2022-09-19 13:08:00 -0700

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,15 +2,10 @@
 
 
 /**
- *
  * swap_int - swaps two integers' values
- *
  * @a: first integer
- *
  * @b: second integer
- *
  * Return: returns nothing
- *
  */
 
 void swap_int(int *a, int *b)
