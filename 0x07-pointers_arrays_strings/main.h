e9c7c2cde4fd9819454c9c501016603a937d5f3c
edmundspira <edmunddspira@gmail.com>
2022-09-26 11:56:00 -0700

    TASK 7

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,6 +13,7 @@ void _puts(char *str);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
+void print_chessboard(char (*a)[8]);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
