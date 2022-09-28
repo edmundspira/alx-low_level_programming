23ca508080d9ea77356e4726a3b65fe9802c9131
edmundspira <edmunddspira@gmail.com>
2022-09-28 13:27:07 -0700

    TASK 6

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -57,3 +57,19 @@ int _strlen_recursion(char *s)
 	s++;
 	return (_strlen_recursion(s) + 1);
 }
+
+int is_palindrome(char *);
+
+/**
+ * main - check the code
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int r;
+
+	r = is_palindrome("I guess I thought the dream-space would be all about the visual, but it's more about the feeling. My question is what happens when you start messing with the physics of it.ti fo scisyhp eht htiw gnissem trats uoy nehw sneppah tahw si noitseuq yM .gnileef eht tuoba erom s'ti tub ,lausiv eht tuoba lla eb dluow ecaps-maerd eht thguoht I sseug I");
+	printf("%d\n", r);
+	return (0);
+}
