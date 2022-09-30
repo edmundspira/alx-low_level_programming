d026ccff8242f6dcdd877d6444cb04c767842f10
edmundspira <edmunddspira@gmail.com>
2022-09-30 02:30:28 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints its name
+ * @argv: argument vector
+ * @argc: argument count
+ *
+ * Return: Always zero
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
