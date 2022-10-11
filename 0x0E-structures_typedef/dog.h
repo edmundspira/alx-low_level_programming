d98e3ec21a8756e18bab0b6cbdf29ca0ff4ac932
edmundspira <edmunddspira@gmail.com>
2022-10-11 12:59:52 -0700

    dog.h fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,6 @@
 #ifndef _HEADER_
 #define _HEADER_
-#include <stdio.h>
-#include "dog.h"
+
 
 /**
  * struct dog - Dog attributes
@@ -24,9 +23,10 @@ struct dog
  */
 
 typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+{
+	void init_dog(struct dog *d, char *name, float age, char *owner);
+	void print_dog(struct dog *d);
+	dog_t *new_dog(char *name, float age, char *owner);
+	void free_dog(dog_t *d);
+}
 #endif
